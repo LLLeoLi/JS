ec1c5a5cdaf49b040136a175a6f669dab9879de6
李昊 <1097261154@qq.com>
2023-02-03 15:47:26 +0800

    update: 无原型对象

--- 15_plainObject.js
+++ 15_plainObject.js
@@ -1,13 +1,13 @@
-let plainObject = Object.create(null,{
+let plainObject = Object.create(null, {
     // 用描述器创建属性时，它的标识默认是false
-    toString:{
-        value: function(){
+    toString: {
+        value: function () {
             return Object.keys(this).join();
         }
     }
 });
 plainObject.apple = "Apple";
 plainObject.__proto__ = "test";
-for(let key in plainObject) {
+for (let key in plainObject) {
     console.log(key); // "apple"，然后是 "__proto__"
-  }
+}
